08d6e6cf6f65655f31bf5342b95b1466faa9dc4a
Mahammad Shareef Syed <mdshareef1911@gmail.com>
2026-05-05 17:02:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,11 +5,15 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' }
 });
 
-// Attach token
+// Attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 // Handle 401 globally
